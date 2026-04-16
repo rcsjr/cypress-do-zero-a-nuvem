c152862b3f8d72e25c0600590576d32a41befb72
Robson Junior <robson.csjunior1@gmail.com>
2026-04-16 15:46:01 -0300

    Finalizando cypress-do-zero-a-nuvem1

--- cypress/e2e/privacyPolicy.cy.js
+++ cypress/e2e/privacyPolicy.cy.js
@@ -6,10 +6,3 @@
     cy.contains('h1','CAC TAT - Política de Privacidade').should('be.visible')
     cy.contains('p', 'Talking About Testing').should('be.visible')
 })
-
-//Aula 13
-  //Exercício extra 4
-  it.only('faz uma requisição HTTP', () =>{
-  cy.request('https://cac-tat-v3.s3.eu-central-1.amazonaws.com/index.html')
-    .should('Get', '200')
-})
